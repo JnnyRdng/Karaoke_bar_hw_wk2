15b5889a099698fb353fdf7a67dcd8b74ca9c1c4
Jonny Reading <jonnyreading@gmail.com>
2020-07-24 16:25:14 +0100

    Room has an empty playlist

--- tests/room_test.py
+++ tests/room_test.py
@@ -2,10 +2,18 @@ import unittest
 
 from src.room import Room
 
+
 class TestRoom(unittest.TestCase):
-    
-    def test_room_has_no_guests(self):
-        room = Room()
+    def setUp(self):
+        self.empty_room = Room()
+
+    def test_empty_room_has_no_guests(self):
         expected = []
-        actual = room.guests
+        actual = self.empty_room.guests
         self.assertEqual(expected, actual)
+
+    def test_empty_room_has_no_songs(self):
+        expected = []
+        actual = self.empty_room.playlist
+        self.assertEqual(expected, actual)
+
