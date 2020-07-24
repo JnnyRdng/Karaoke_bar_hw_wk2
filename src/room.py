896c2ab6062db250f688d68114b8de7bfbf9caa1
Jonny Reading <jonnyreading@gmail.com>
2020-07-24 17:51:49 +0100

    room method .start_playlist() can add to the .playlist list

--- src/room.py
+++ src/room.py
@@ -2,3 +2,7 @@ class Room:
     def __init__(self):
         self.guests = []
         self.playlist = []
+
+    def start_playlist(self, playlist):
+        self.playlist = [1,2,3,4,5]
+    
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,11 +1,19 @@
 import unittest
 
+from src.song import Song
 from src.room import Room
 
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
         self.empty_room = Room()
+        self.songs = [
+            Song("Africa", "Toto"),
+            Song("Don't Stop Believin'", "Journey"),
+            Song("Wonderwall", "Oasis"),
+            Song("Uptown Girl", "Billy Joel"),
+            Song("Ring of Fire", "Johnny Cash"),
+        ]
 
     def test_empty_room_has_no_guests(self):
         expected = []
@@ -15,5 +23,11 @@ class TestRoom(unittest.TestCase):
     def test_empty_room_has_no_songs(self):
         expected = []
         actual = self.empty_room.playlist
+        self.assertEqual(expected, actual)
+
+    def test_set_playlist_for_room__get_length(self):
+        self.empty_room.start_playlist(self.songs)
+        expected = 5
+        actual = len(self.empty_room.playlist)
         self.assertEqual(expected, actual)
 
