d088829ecd8468ffee7c06f8c12d38e3c5f32875
Jonny Reading <jonnyreading@gmail.com>
2020-07-24 16:35:49 +0100

    Song has a title

--- tests/song_test.py
+++ tests/song_test.py
@@ -2,5 +2,11 @@ import unittest
 
 from src.song import Song
 
+
 class TestSong(unittest.TestCase):
-    pass
+    def test_song_has_title(self):
+        song = Song("Deep Blue")
+        expected = "Deep Blue"
+        actual = song.title
+        self.assertEqual(expected, actual)
+
