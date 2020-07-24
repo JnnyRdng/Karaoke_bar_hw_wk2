3518895a5252c323e857971702458740783224d4
Jonny Reading <jonnyreading@gmail.com>
2020-07-24 15:54:43 +0100

    Guest has a name

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -3,4 +3,9 @@ import unittest
 from src.guest import Guest
 
 class TestGuest(unittest.TestCase):
-    pass
+    
+    def test_can_make_a_guest_with_name(self):
+        guest = Guest("Rick")
+        expected = "Rick"
+        actual = guest.name
+        self.assertEqual(expected, actual)
