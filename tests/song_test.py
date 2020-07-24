9f43ad030c48a560acee1ab4ba1bcb82150acb5e
Jonny Reading <jonnyreading@gmail.com>
2020-07-24 16:37:38 +0100

    Song has an artist

--- tests/song_test.py
+++ tests/song_test.py
@@ -5,8 +5,13 @@ from src.song import Song
 
 class TestSong(unittest.TestCase):
     def test_song_has_title(self):
-        song = Song("Deep Blue")
+        song = Song("Deep Blue", "Arcade Fire")
         expected = "Deep Blue"
         actual = song.title
         self.assertEqual(expected, actual)
 
+    def test_song_has_an_artist(self):
+        song = Song("Deep Blue", "Arcade Fire")
+        expected = "Arcade Fire"
+        actual = song.artist
+        self.assertEqual(expected, actual)
