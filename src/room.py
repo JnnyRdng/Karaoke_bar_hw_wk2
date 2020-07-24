a50ea518bd9b52c93083fee13e4c6a1baf731cf9
Jonny Reading <jonnyreading@gmail.com>
2020-07-24 18:51:03 +0100

    Room can add guest to guests list

--- src/room.py
+++ src/room.py
@@ -6,3 +6,5 @@ class Room:
     def start_playlist(self, playlist):
         self.playlist += playlist
     
+    def add_guest_to_room(self, guest):
+        self.guests.append(guest)
--- tests/room_test.py
+++ tests/room_test.py
@@ -2,11 +2,12 @@ import unittest
 
 from src.song import Song
 from src.room import Room
+from src.guest import Guest
 
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
-        self.empty_room = Room()
+        self.room = Room()
         self.songs = [
             Song("Africa", "Toto"),
             Song("Don't Stop Believin'", "Journey"),
@@ -14,26 +15,33 @@ class TestRoom(unittest.TestCase):
             Song("Uptown Girl", "Billy Joel"),
             Song("Ring of Fire", "Johnny Cash"),
         ]
+        self.guest = Guest("Timmy")
 
     def test_empty_room_has_no_guests(self):
         expected = []
-        actual = self.empty_room.guests
+        actual = self.room.guests
         self.assertEqual(expected, actual)
 
     def test_empty_room_has_no_songs(self):
         expected = []
-        actual = self.empty_room.playlist
+        actual = self.room.playlist
         self.assertEqual(expected, actual)
 
     def test_set_playlist_for_room__get_length(self):
-        self.empty_room.start_playlist(self.songs)
+        self.room.start_playlist(self.songs)
         expected = 5
-        actual = len(self.empty_room.playlist)
+        actual = len(self.room.playlist)
         self.assertEqual(expected, actual)
 
     def test_playlist_items_are_song_objects(self):
-        self.empty_room.start_playlist(self.songs)
+        self.room.start_playlist(self.songs)
         expected = "Uptown Girl"
-        actual = self.empty_room.playlist[3].title
+        actual = self.room.playlist[3].title
+        self.assertEqual(expected, actual)
+
+    def test_add_guest_to_room(self):
+        self.room.add_guest_to_room(self.guest)
+        expected = 1
+        actual = len(self.room.guests)
         self.assertEqual(expected, actual)
 
